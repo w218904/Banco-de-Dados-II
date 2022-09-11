086b43c90216ae3399f2beac919362aa70d6854b
w218904 <61876667+w218904@users.noreply.github.com>
2022-09-11 16:53:13 -0300

    Add files via upload

---
+++ exercicio3.sql
@@ -0,0 +1,45 @@
+--garantindo que estamos no banco master
+use master
+go
+
+--criação do banco de dados principal
+create database exercicio3
+go
+
+--garantindo que estamos no banco necessário
+use exercicio3
+
+create table produto (
+	codproduto int not null,
+	nome char (20) not null,
+	preco numeric (6,2),
+	qtdestoque int,
+	primary key (codproduto)
+)
+go
+
+create table notafiscal (
+	numnota int not null,
+	valortotal numeric (6,2) not null,
+	primary key (numnota)
+)
+go
+
+create table itemnotafiscal (
+	numnota int not null,
+	codproduto int not null,
+	quantidade int not null,
+	primary key (codproduto, numnota),
+	foreign key (codproduto) references produto,
+	foreign key (numnota) references notafiscal,
+)
+
+create table fatura (
+	numfatura int not null,
+	dtvencimento date not null,
+	dtpagamento date not null,
+	valor numeric (6,2) not null,
+	numnota int not null,
+	primary key (numfatura),
+	foreign key (numnota) references notafiscal
+)
