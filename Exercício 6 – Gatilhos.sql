191f4da6bc5b88a6584ef82f9d26310c509d9612
Tauan Rodrigues <Tauan.rod@gmail.com>
2022-10-26 16:29:24 -0300

    feitas as questões A e B e parcialmente a C

--- Exercício 6 – Gatilhos.sql
+++ Exercício 6 – Gatilhos.sql
@@ -1,9 +1,11 @@
---Apagando o banco de dados antigo e criando um novo
+--Apagando o banco de dados antigo
 use master
 go
 
 drop database st767
 go
+
+--criando um banco de dados novo
 
 use master
 go
@@ -15,7 +17,7 @@ go
 use st767
 go
 
-create table pproduto (
+create table produto (
 codproduto int not null,
 nome varchar(60) not null,
 preco numeric(5,2) not null,
@@ -37,7 +39,7 @@ codproduto int not null,
 quantidade int not null,
 primary key (numnota,codproduto),
 foreign key (numnota) references notafiscal,
-foreign key (codproduto) references pproduto
+foreign key (codproduto) references produto
 )
 go
 
@@ -60,39 +62,156 @@ create index ixfatura_numnota on fatura(numnota)
 go
 
 --Inserindo dados para teste
-insert into pproduto values (1, 'lapis', 1.00, 100)
-insert into pproduto values (2, 'caneta', 1.50, 100)
-insert into pproduto values (3, 'borracha', 1.00, 100)
-insert into pproduto values (4, 'papel', 0.50, 100)
+insert into produto values (1, 'lapis', 1.00, 100)
+insert into produto values (2, 'caneta', 1.50, 100)
+insert into produto values (3, 'borracha', 1.00, 100)
+insert into produto values (4, 'papel', 0.50, 100)
 
-insert into notafiscal values (1940028922, 0)
-
-go
-
---Selects para teste
-select * from pproduto
-select * from notafiscal
-select * from itemnotafiscal
+insert into notafiscal values (1, 0)
 go
 
 --Criação dos gatilhos
+use st767
+go
 create trigger inclusaoitemnotafiscal
 on itemnotafiscal for insert
 as
 begin
-	update pproduto 
+	update produto 
 	set qtdestoque = qtdestoque - (select quantidade from inserted)
+		where codproduto = (select codproduto from inserted)
 	if @@ROWCOUNT = 0
 	rollback transaction
 end
 begin
 	update notafiscal
-	set valortotal = valortotal +  (select p.preco * (i.quantidade - d.quantidade)
-									from pproduto p inner join inserted i
-									on p.codproduto = i.codproduto
-									inner join deleted d
-									on i.codproduto = d.codproduto and
-									i.numnota = d.numnota)
+	set valortotal = valortotal +  (select p.preco * i.quantidade
+									from produto p inner join inserted i
+									on p.codproduto = i.codproduto)
 	if @@ROWCOUNT = 0
 	rollback transaction
 end
+go
+
+create trigger exclusaoitemnotafiscal
+on itemnotafiscal for delete
+as
+begin
+	update produto 
+	set qtdestoque = qtdestoque + (select quantidade from deleted)
+		where codproduto = (select codproduto from deleted)
+	if @@ROWCOUNT = 0
+	rollback transaction
+end
+begin
+	update notafiscal
+	set valortotal = valortotal -  (select p.preco * d.quantidade
+									from produto p inner join deleted d
+									on p.codproduto = d.codproduto)
+	if @@ROWCOUNT = 0
+	rollback transaction
+end
+go
+
+create trigger atualizaitemnotafiscal
+on itemnotafiscal for update
+as
+if update (quantidade)
+begin
+	update produto 
+	set qtdestoque = qtdestoque - (select (i.quantidade - d.quantidade)
+									from produto p inner join inserted i
+									on p.codproduto = i.codproduto
+									inner join deleted d
+									on i.codproduto = d.codproduto)
+		where codproduto = (select codproduto from deleted)
+	if @@ROWCOUNT = 0
+	rollback transaction
+	else
+	begin
+		update notafiscal
+		set valortotal = valortotal + (select p.preco * (i.quantidade - d.quantidade)
+										from produto p inner join inserted i
+										on p.codproduto = i.codproduto
+										inner join deleted d
+										on i.codproduto = d.codproduto and
+										i.numnota = d.numnota)
+			where numnota = (select codproduto from inserted)
+		if @@ROWCOUNT = 0
+		rollback transaction
+	end
+end
+go
+
+--Inserções para teste
+insert into itemnotafiscal values (1, 2, 10)
+insert into itemnotafiscal values (1, 3, 2)
+insert into itemnotafiscal values (1, 1, 2)
+insert into itemnotafiscal values (1, 4, 5)
+go
+
+--Selects para teste
+select * from produto
+select * from notafiscal
+select * from itemnotafiscal
+go
+
+--Apagar Gatilho
+use st767
+go
+drop trigger inclusaoitemnotafiscal, exclusaoitemnotafiscal
+go
+drop trigger atualizaitemnotafiscal
+go
+
+--deletes para teste
+delete from itemnotafiscal where codproduto = 1
+delete from itemnotafiscal where codproduto = 2
+delete from itemnotafiscal where codproduto = 3
+delete from itemnotafiscal where codproduto = 4
+go
+
+--updates para teste
+update itemnotafiscal
+	set quantidade = 20 
+	where codproduto = 1
+go
+update itemnotafiscal
+	set quantidade = 20 
+	where codproduto = 2
+go
+update itemnotafiscal
+	set quantidade = 20 
+	where codproduto = 3
+go
+update itemnotafiscal
+	set quantidade = 20 
+	where codproduto = 4
+go
+
+--Deletes para as rows de itemnotafiscal
+delete from itemnotafiscal
+where numnota = 1
+delete from itemnotafiscal
+where numnota = 2
+delete from itemnotafiscal
+where numnota = 3
+delete from itemnotafiscal
+where numnota = 4
+go
+
+--Selects para teste
+select * from produto
+select * from notafiscal
+select * from itemnotafiscal
+go
+
+--drop geral
+use st767
+go
+drop table itemnotafiscal
+go
+drop table fatura
+go
+drop table produto, notafiscal
+go
